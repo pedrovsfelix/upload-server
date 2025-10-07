33b6adc4c2d432f927ac657828b666dc05f4461c
pedrovsfelix <pedrovitorsf@gmail.com>
2025-10-07 06:20:00 -0300

    :sparkles: feat: Criando rota de upload da imagem.

--- src/infra/http/routes/upload_image.ts
+++ src/infra/http/routes/upload_image.ts
@@ -1,21 +1,57 @@
-import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
-import { z } from "zod";
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { uploadImage } from '@/app/functions/upload-image'
+import { isRight, unwrapEither } from '@/shared/either'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
-    await server.post('/uploads', {
-        schema: {
-            summary: 'Upload an image.',
-            body: z.object({
-                name: z.string(),
-                password: z.string().optional()
-            }),
+  await server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image.',
+        consumes: ['multipart/form-data'],
 
-            response: {
-                201: z.object({ uploadId: z.string() }),
-                // 409: z.object({ message: z.string() }).describe('Upload already exists.'),
-            }
+        response: {
+          201: z.null().describe('Image uploaded'),
+          400: z.object({ message: z.string() }),
         },
-    }, async (request, reply) => {
-        return reply.status(201).send({ uploadId: 'teste' })
-    })
+      },
+    },
+    async (request, reply) => {
+      const uploadedFile = await request.file({
+        limits: {
+          fileSize: 1024 * 1024 * 2,
+        },
+      })
+
+      if (!uploadedFile) {
+        return reply.status(400).send({ message: 'File is required.' })
+      }
+
+      const result = await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      })
+
+      if (uploadedFile.file.truncated) {
+        return reply.status(400).send({
+          message: 'File size limit reached.',
+        })
+      }
+
+      if (isRight(result)) {
+        console.log(unwrapEither(result))
+
+        return reply.status(201).send()
+      }
+
+      const error = unwrapEither(result)
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormat':
+          return reply.status(400).send({ message: error.message })
+      }
+    }
+  )
 }
